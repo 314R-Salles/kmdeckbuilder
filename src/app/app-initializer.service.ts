20834a5f18d8672d292d6159c52b25bfe3b88492
314R-Salles <sallespierre@live.fr>
2026-08-29 23:57:49 +0200

    désactiver la verif obligatoire pour le deckbuilder

--- src/app/app-initializer.service.ts
+++ src/app/app-initializer.service.ts
@@ -32,11 +32,11 @@ export class AppInitializerService {
       if (token) {
         this.authenticatedApiService.linkAccount(token).subscribe(user => this.storeService.setUser(user))
       }
-      this.route.queryParamMap.subscribe(params => {
-        if (params.get("verified")) {
-          this.authenticatedApiService.refreshMailStatus().subscribe(user => this.storeService.setUser(user))
-        }
-      })
+      // this.route.queryParamMap.subscribe(params => {
+      //   if (params.get("verified")) {
+      //     this.authenticatedApiService.refreshMailStatus().subscribe(user => this.storeService.setUser(user))
+      //   }
+      // })
       return this.apiService.getCardIllustrations().pipe(
         switchMap(cardList => {
           this.storeService.setCardIllustrations(cardList)
